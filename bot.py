3c7f93eb6943d8ce834aa8d172bead830c6d2c97
Cntrfct <zef.frz@gmail.com>
2023-07-16 19:07:54 +0400

    bot complete

---
+++ bot.py
@@ -0,0 +1,38 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config.config import Config, load_config
+from handlers import other_handlers, user_handlers
+
+# Создаем логгер
+logger = logging.getLogger(__name__)
+
+
+# Функция конфигурации и запуска бота
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+    # Выводим в консоль инфо о запуске бота
+    logger.info('Starting bot...')
+
+    # Загружаем конфиг в config
+    config: Config = load_config()
+
+    # Создаём бот и диспетчер
+    bot: Bot = Bot(token=config.tg_bot.token,
+                   parse_mode='HTML')
+    dp: Dispatcher = Dispatcher()
+
+    # Регистрируем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    # Пропускаем апдейты и запускаем поллинг
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Rock, Paper, Scissors bot for Telegram
 ## Using Python and AIOgram 3
-### Very simple script
+#### Very simple script
